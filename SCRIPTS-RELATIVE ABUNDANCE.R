862eee20d4663ddaf1a462fa2f7da5748e4f9e01
vigneshpals <117217125+vigneshpals@users.noreply.github.com>
2023-06-29 11:00:05 -0500

    Update and rename SCRIPTS-RELATIVE ABUNDANCE.txt to SCRIPTS-RELATIVE ABUNDANCE.R

--- SCRIPTS-RELATIVE ABUNDANCE.R
+++ SCRIPTS-RELATIVE ABUNDANCE.R
@@ -1,24 +1,27 @@
-prune.dat <- prune_taxa(taxa_sums(ww1) > 0, ww1)
+#R Script used to generate Relative Abundance Profiles
+#Input from the Phyloseq Main Code file
 
-top20 <- names(sort(taxa_sums(prune.dat), decreasing=TRUE)[1:50])
+prune_ww <- prune_taxa(taxa_sums(ww1) > 0, ww1)
+
+top20 <- names(sort(taxa_sums(prune_ww), decreasing=TRUE)[1:50])
 top20 #shows 20 results
 
-dat.aglo = tax_glom(prune.dat, taxrank = "Family")
-dat.trans = transform_sample_counts(dat.aglo, function(x) x/sum(x))
+glom_ww = tax_glom(prune_ww, taxrank = "Family")
+trans_data = transform_sample_counts(glom_ww, function(x) x/sum(x))
 
-prune.dat.two = prune_taxa(top20, dat.trans)
-dat.dataframe = psmelt(prune.dat.two)
+data_prune = prune_taxa(top20, trans_data)
+df = psmelt(data_prune)
 
-ps1 <- merge_samples(prune.dat.two, "Plant")
+df1 <- merge_samples(data_prune, "Plant")
 
-ps2 <- transform_sample_counts(ps1, function(x) x / sum(x))
+df2 <- transform_sample_counts(df1, function(x) x / sum(x))
 
 png("Family-Plants.png",
     width = 20*300,
     height = 20*300,
     res = 600,
     pointsize = 12)
-plot_bar(ps2, fill="Family", title = "Family-Plants") + geom_bar(aes(color=Family, fill=Family), colour = "black" , stat="identity", position="stack") + 
+plot_bar(df2, fill="Family", title = "Family-Plants") + geom_bar(aes(color=Family, fill=Family), colour = "black" , stat="identity", position="stack") + 
   scale_color_viridis(discrete = TRUE)
 dev.off()
 
